f211897e2de2ce831863fe535ad4d1f6566128e9
Michael Riley <mwriley1@gmail.com>
2016-06-07 16:37:09 -0400

    Updated 21.rb

--- lesson5/21.rb
+++ lesson5/21.rb
@@ -1,18 +1,4 @@
-module Hand
-  def hit
-  end
-
-  def stay
-  end
-
-  def busted?
-  end
-
-  def total
-    # definitely looks like we need to know about "cards" to produce some total
-  end
-end
-
+# frozen_string_literal:true
 
 class Participant
   attr_accessor :name, :hand
@@ -20,29 +6,89 @@ class Participant
   def initialize
     @hand = []
   end
+
+  def display_hand
+    hand.map(&:to_s).join(', ')
+  end
+
+  def stay
+    puts self.class == Player ? "You have chosen to stay." : "The Dealer stays."
+    puts ""
+  end
+
+  def busted?
+    card_total > 21
+  end
+
+  def card_total
+    hand_total = 0
+
+    hand.each do |card|
+      hand_total += 10 if ['King', 'Queen', 'Jack'].include?(card.value)
+      hand_total += 11 if card.value == 'Ace'
+      hand_total += card.value.to_i
+    end
+
+    hand.select { |card| card.value == 'Ace' }.count.times do
+      hand_total -= 10 if hand_total > Game::WIN_TOTAL
+    end
+
+    hand_total
+  end
 end
 
 class Player < Participant
-  include Hand
-
-  def initialize
-    @name = "Player"
-    super
-  end 
 end
 
 class Dealer < Participant
-  include Hand
+  attr_reader :deck, :player
 
   def initialize
+    @deck = Deck.new
+    @player = Player.new
     @name = ['Bob', 'Joe', 'Luke', 'Penny', 'Sarah', 'Rachel'].sample
     super
+  end
+
+  def shuffle
+    deck.cards.shuffle!
+  end
+
+  def deal
+    2.times do |_|
+      player.hand << deck.deal_a_card
+      hand << deck.deal_a_card
+    end
+  end
+
+  def administer_hit
+    puts "You have chosen to hit."
+    puts ""
+    player.hand << deck.deal_a_card
+  end
+
+  def hit
+    puts "#{name} has chosen to hit."
+    puts ""
+    hand << deck.deal_a_card
+  end
+
+  def display_visible_hand
+    hand_string = hand.map(&:to_s)
+    hand_string[0] = '?'
+    hand_string.join(', ')
+  end
+
+  def return_cards(hand1, hand2)
+    deck.cards += (hand1 + hand2)
   end
 end
 
 class Card
-  SUITS = %w(Hearts Diamonds Clubs Spades)
-  VALUES = %w(Ace King Queen Jack Ten Nine Eight Seven Six Five Four Three Two)
+  SUITS = %w(Hearts Diamonds Clubs Spades).freeze
+  VALUES = %w(Ace King Queen Jack 10 9 8 7 6 5 4 3 2).freeze
+
+  attr_reader :value
 
   def initialize(value = VALUES.sample, suit = SUITS.sample)
     @value = value
@@ -55,46 +101,57 @@ class Card
 end
 
 class Deck
-  DECK_CARDS = (Card::VALUES.product(Card::SUITS)).freeze
+  DECK_CARDS = Card::VALUES.product(Card::SUITS).freeze
 
   attr_accessor :cards
 
   def initialize
-    @cards = []
-    fill
-    shuffle
+    @cards = add_all_cards
   end
 
-  def fill
+  def add_all_cards
+    deck = []
     DECK_CARDS.each do |card|
-      cards << Card.new(*card)
+      deck << Card.new(*card)
     end
+    deck
   end
 
-  def deal
-    cards.pop
+  def deal_a_card
+    cards.shift
   end
 
-  def shuffle
-    cards.shuffle!
+  def to_s
+    cards.collect(&:to_s)
   end
 end
 
 class Game
+  WIN_TOTAL = 21
+
+  attr_reader :dealer, :player
+
   def initialize
-    @deck = Deck.new
-    @player = Player.new
     @dealer = Dealer.new
+    @player = dealer.player
   end
 
   def play
     display_welcome_message
-    deal_cards
-    show_initial_cards
-    # player_turn
-    # dealer_turn
-    # show_result
-    # play_again?
+    set_player_name
+
+    loop do
+      deal_cards
+      display_visible_cards
+
+      player_turn
+      dealer_turn
+
+      display_result
+      game_reset
+      break unless play_again?
+    end
+
     display_goodbye_message
   end
 
@@ -103,16 +160,103 @@ class Game
     puts ""
   end
 
+  def set_player_name
+    answer = ""
+    loop do
+      puts "Please enter your name."
+      answer = gets.chomp
+      break if answer != ""
+      puts "Sorry, you must enter something."
+    end
+    player.name = answer
+  end
+
   def deal_cards
-    2.times do |_|
-      @player.hand << @deck.deal
-      @dealer.hand << @deck.deal
+    system 'clear'
+    dealer.shuffle
+    puts "Dealing a new hand..."
+    puts ""
+    dealer.deal
+  end
+
+  def display_visible_cards
+    puts "#{player.name}'s Cards: [#{player.display_hand}]  Total: #{player.card_total}"
+    puts "#{dealer.name}'s Cards (Dealer): [#{dealer.display_visible_hand}]  Card Total: ?"
+    puts ""
+  end
+
+  def display_all_cards
+    puts "#{player.name}'s Cards: [#{player.display_hand}]  Total: #{player.card_total}"
+    puts "#{dealer.name}'s Cards (Dealer): [#{dealer.display_hand}] Total: #{dealer.card_total}"
+    puts ""
+  end
+
+  def player_turn
+    until player.busted?
+      puts "Would you like to hit or stay? (h / s)"
+      answer = nil
+      loop do
+        answer = gets.chomp.downcase
+        break if ['h', 's'].include?(answer)
+        puts "Sorry, that is not a valid choice."
+      end
+
+      system 'clear'
+      
+      if answer == 'h'
+        dealer.administer_hit
+        display_visible_cards
+      else
+        player.stay
+        display_all_cards
+        break
+      end
     end
   end
 
-  def show_initial_cards
-    puts "Player Cards: [#{@player.hand[0]}, ?]" 
-    puts "Dealer Cards [#{@dealer.hand[0]}, ?]"
+  def dealer_turn
+    until player.busted?
+      if dealer.card_total < 17
+        dealer.hit
+        display_all_cards
+        break if dealer.busted?
+      else
+        dealer.stay
+        break
+      end
+    end
+  end
+
+  def display_result
+    if player.busted?
+      puts "You have busted.  You lose!"
+    elsif dealer.busted?
+      puts "The dealer has busted.  You win!"
+    elsif player.card_total > dealer.card_total
+      puts "You win!"
+    elsif player.card_total < dealer.card_total
+      puts "You lose!"
+    else
+      puts "It's a push!"
+    end
+  end
+
+  def play_again?
+    answer = nil
+
+    loop do
+      puts "Would you like to play again?"
+      answer = gets.chomp.downcase
+      break if ['y', 'n'].include?(answer)
+      puts "Sorry, you must enter 'y' or 'n'."
+    end
+    answer == 'y'
+  end
+
+  def game_reset
+    dealer.return_cards(player.hand, dealer.hand)
+    player.hand = []
+    dealer.hand = []
   end
 
   def display_goodbye_message
@@ -121,11 +265,3 @@ class Game
 end
 
 Game.new.play
-
-
-
-
-
-
-
-
